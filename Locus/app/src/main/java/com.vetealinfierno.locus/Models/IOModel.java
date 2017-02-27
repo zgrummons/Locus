687b9516922109cbd9345d5eb68c18cdb5919fe0
Zachariah Grummons <zachariah.grummons@ttu.edu>
2017-02-27 02:17:22 -0600

    added models and migrated logic to them
    todo: async the shit so android stops throwing network errors

---
+++ Locus/app/src/main/java/com.vetealinfierno.locus/Models/IOModel.java
@@ -0,0 +1,33 @@
+import android.app.Activity;
+import android.content.SharedPreferences;
+
+/**
+ * Created by zgrum on 2/27/2017.
+ */
+
+public class IOModel {
+
+    public static final String PREFS_NAME = "LOCUS_Config";
+
+    protected static void loadConfig (Activity context)
+    {
+        DBModel dbModel = DBModel.getInstance();
+        SharedPreferences settings = context.getSharedPreferences(PREFS_NAME, 0);
+        if (!settings.contains("UserId"))
+            dbModel.setUserId(settings.getInt("UserId", -1));
+
+        if (dbModel.getUserId() == -1)
+            DBModel.getNewUserId();
+        else
+            DBModel.getGroupId();
+    }
+
+    protected static void saveConfig (Activity context)
+    {
+        DBModel dbModel = DBModel.getInstance();
+        SharedPreferences settings = context.getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt("UserId", dbModel.getUserId());
+        editor.commit();
+    }
+}
